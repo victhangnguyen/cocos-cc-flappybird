4834994f54472a1ab22531e1add82b328380e4c6
victhangnguyen <victhangnguyen@outlook.com>
2024-09-29 03:12:44 +0700

    create Ground Scripts, adding UpdateEvent to moving Ground.

--- assets/Scripts/Ground.ts
+++ assets/Scripts/Ground.ts
@@ -1,14 +1,94 @@
-import { _decorator, Component, Node } from 'cc';
+import {
+  _decorator,
+  Canvas,
+  Component,
+  director,
+  Node,
+  UITransform,
+  Vec3,
+} from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('Ground')
+@ccclass("Ground")
 export class Ground extends Component {
-    start() {
+  @property({
+    type: Node,
+    tooltip: "Ground 1 is here",
+  })
+  public ground1: Node;
 
-    }
+  @property({
+    type: Node,
+    tooltip: "Ground 2 is here",
+  })
+  public ground2: Node;
 
-    update(deltaTime: number) {
-        
+  @property({
+    type: Node,
+    tooltip: "Ground 3 is here",
+  })
+  public ground3: Node;
+
+  //! Create ground width variables
+  public groundWidth1: number;
+  public groundWidth2: number;
+  public groundWidth3: number;
+
+  //! Temp variable to store start location of ground
+  public tempStartLocation1 = new Vec3();
+  public tempStartLocation2 = new Vec3();
+  public tempStartLocation3 = new Vec3();
+
+  //! Game speed
+  public gameSpeed: number = 50;
+
+  onLoad() {
+    this.startUp();
+    // this.groundWidth1 = this.ground1.getComponent(BoxCollider).size.x;
+    // this.groundWidth2 = this.ground2.getComponent(BoxCollider).size.x;
+    // this.groundWidth3 = this.ground3.getComponent(BoxCollider).size.x;
+  }
+
+  startUp() {
+    //! this trỏ ground1 trỏ Component
+    this.groundWidth1 = this.ground1.getComponent(UITransform).width;
+    this.groundWidth2 = this.ground2.getComponent(UITransform).width;
+    this.groundWidth3 = this.ground2.getComponent(UITransform).width;
+
+    this.tempStartLocation1.x = 0;
+    this.tempStartLocation2.x = this.groundWidth1;
+    this.tempStartLocation3.x = this.groundWidth1 + this.groundWidth2;
+
+    //! setPosition
+
+    this.ground1.setPosition(this.tempStartLocation1);
+    this.ground2.setPosition(this.tempStartLocation2);
+    this.ground3.setPosition(this.tempStartLocation3);
+  }
+
+  update(deltaTime: number) {
+    this.tempStartLocation1 = this.ground1.position;
+    this.tempStartLocation2 = this.ground2.position;
+    this.tempStartLocation3 = this.ground3.position;
+    //! part 3: adding a moveing ground
+    //! get the speed and subtract from x
+    this.tempStartLocation1.x -= this.gameSpeed * deltaTime;
+    this.tempStartLocation2.x -= this.gameSpeed * deltaTime;
+    this.tempStartLocation3.x -= this.gameSpeed * deltaTime;
+    const scene = director.getScene();
+    const canvas = scene.getComponentInChildren(Canvas);
+    //! set the position
+    if (this.tempStartLocation1.x <= 0 - this.groundWidth1) {
+      this.tempStartLocation1.x = canvas.getComponent(UITransform).width;
     }
+    if (this.tempStartLocation2.x <= 0 - this.groundWidth2) {
+      this.tempStartLocation2.x = canvas.getComponent(UITransform).width;
+    }
+    if (this.tempStartLocation3.x <= 0 - this.groundWidth3) {
+      this.tempStartLocation3.x = canvas.getComponent(UITransform).width;
+    }
+    this.ground1.setPosition(this.tempStartLocation1);
+    this.ground2.setPosition(this.tempStartLocation2);
+    this.ground3.setPosition(this.tempStartLocation3);
+  }
 }
-
